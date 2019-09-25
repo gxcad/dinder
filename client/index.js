dc7689a8c9f80860f51c568a144629a698dde813
Benjamin L. Morrison <newben.hd@gmail.com>
2019-09-25 14:45:51 -0700

    added redux and redux-thunk to react

--- client/index.js
+++ client/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App.jsx';
-import normalize from './styles/normalize.css';
-import styles from './styles/styles.scss';
+import './styles/normalize.css';
+import './styles/styles.scss';
 
-render(<App />, document.getElementById('app'));
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from './reducers/reducer';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+
+// enable redux chrome dev tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// thunkMiddleware allows use dispatch() functions in child components
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+// wrap react app with Provider to connect reducers with child components
+render(<Provider store={store}><App /></Provider>,
+  document.getElementById('app'));
