efed578ee9d6af4337f22c3c203874e62387c5df
Ke Chen <keke@Kes-MacBook-Air.local>
2019-09-26 12:29:06 -0700

    css

--- client/components/Resturant.jsx
+++ client/components/Resturant.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 export default props => {
     const { name, url, image_url } = props;
     return (
-        <div className='restaurantCard'>
+        <div className='resturantCard'>
             <h3>{name}</h3>
             <a href={url}>Link</a>
             <img src={image_url} alt="Image" />
